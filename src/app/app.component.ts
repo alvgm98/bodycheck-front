ccc7bee24e7fa62c08faa486c60c8140b5ddfcaf
alvgm98 <alvgm1998@gmail.com>
2024-07-08 17:40:15 +0200

    eliminadas importacciones innecesarias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, DashboardComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'bodycheck-front';
+
 }
