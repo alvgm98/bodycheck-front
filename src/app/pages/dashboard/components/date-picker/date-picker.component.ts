026ea047f86fe3e7966b85077f2952df35f237f9
alvgm98 <alvgm1998@gmail.com>
2024-07-09 14:20:48 +0200

    Date-Picker implementado y customizado. Dashboard esquematizado. Customer-List y Agenda creados.

---
+++ src/app/pages/dashboard/components/date-picker/date-picker.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+@Component({
+  selector: 'app-date-picker',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [MatCardModule, MatDatepickerModule],
+  templateUrl: './date-picker.component.html',
+  styleUrl: './date-picker.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DatePickerComponent {
+  selected = model<Date>(new Date);
+}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CustomerListComponent } from './components/customer-list/customer-list.component';
+import { DatePickerComponent } from './components/date-picker/date-picker.component';
+import { AgendaComponent } from './components/agenda/agenda.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CustomerListComponent, DatePickerComponent, AgendaComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
