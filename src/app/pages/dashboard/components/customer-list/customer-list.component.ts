90d3d11038c967676897f454873c392b1cd6a472
alvgm98 <alvgm1998@gmail.com>
2024-07-15 15:49:57 +0200

    Lista de customers iniciada. Paginacion preparada

--- src/app/pages/dashboard/components/customer-list/customer-list.component.ts
+++ src/app/pages/dashboard/components/customer-list/customer-list.component.ts
@@ -1,45 +1,49 @@
-import { Component, effect, AfterViewInit, ViewChild } from '@angular/core';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { AfterViewInit, Component, effect, ElementRef } from '@angular/core';
 import { CustomerService } from '../../../../services/customer.service';
 import { Customer } from '../../../../models/customer';
+import { CapitalizePipe } from '../../../../pipes/capitalize.pipe';
+import { CalculateAgePipe } from '../../../../pipes/calculate-age.pipe';
+import { FilterInputComponent } from '../../../../components/filter-input/filter-input.component';
+import { FilterCustomersPipe } from '../../../../pipes/filter-customers.pipe';
 
 @Component({
   selector: 'app-customer-list',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule],
+  imports: [FilterInputComponent, CapitalizePipe, CalculateAgePipe, FilterCustomersPipe],
   templateUrl: './customer-list.component.html',
   styleUrl: './customer-list.component.scss'
 })
-export class CustomerListComponent {
-  displayedColumns: string[] = ['firstName', 'lastName', 'phone', 'email'];
-  dataSource!: MatTableDataSource<Customer>;
-
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+export class CustomerListComponent implements AfterViewInit {
 
   customers: Customer[] = [];
 
-  constructor(private customerService: CustomerService) {
+  filterCondition: string = "";
+  pageSize: number = 5;
+
+  constructor(
+    private customerService: CustomerService,
+    private el: ElementRef
+  ) {
     this.customerService.loadCustomers();
 
     effect(() => {
       this.customers = customerService.customers();
-      this.dataSource = new MatTableDataSource(this.customers);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
     })
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  ngAfterViewInit(): void {
+    this.calculatePageSize();
+  }
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
+  filterValue(filter: string) {
+    this.filterCondition = filter;
+  }
+
+  calculatePageSize() {
+    const hostHeight = this.el.nativeElement.offsetHeight;
+    const controlsHeight = this.el.nativeElement.querySelector(".controls").offsetHeight;
+    // const paginatorSize = this.el.nativeElement.querySelector(".paginator"); NO OLVIDAR AÑADIR A LA OPERACIÓN
+
+    this.pageSize = Math.floor((hostHeight - controlsHeight - 80) / 80);
   }
 }
