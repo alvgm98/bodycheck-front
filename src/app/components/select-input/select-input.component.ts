11c2dfd6c56102bbf6a8b17bbed069ad8660ff5c
alvgm98 <alvgm1998@gmail.com>
2024-07-25 21:38:22 +0200

    Component Select creado

---
+++ src/app/components/select-input/select-input.component.ts
@@ -0,0 +1,46 @@
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, ElementRef, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-select-input',
+  standalone: true,
+  imports: [NgStyle, NgClass],
+  templateUrl: './select-input.component.html',
+  styleUrl: './select-input.component.scss',
+  host: {
+    '[class.invalid]': 'hasErrors()',
+  }
+})
+export class SelectInputComponent {
+
+  selected: string = '';
+  selectEvent = output<string>();
+  title = input.required<string>();
+  options = input.required<string[]>();
+  hasErrors = input<boolean>(false);
+
+  showDropdown = false;
+
+  constructor(private el: ElementRef) { }
+
+  toggleShowDropdown() {
+    if (!this.showDropdown)
+      this.showDropdown = !this.showDropdown;
+    else
+      this.closeDropdown()
+  }
+
+  closeDropdown() {
+    const dropdown = this.el.nativeElement.querySelector('.dropdown');
+    dropdown.classList.remove('deploy-dropdown-animation')
+    dropdown.classList.add('hide-dropdown-animation')
+
+    setTimeout(() => this.showDropdown = false, 200);
+  }
+
+  select(option: string) {
+    this.selected = option;
+    this.selectEvent.emit(this.selected);
+    this.closeDropdown();
+  }
+}
