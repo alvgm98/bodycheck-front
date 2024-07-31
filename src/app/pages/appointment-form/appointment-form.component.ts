6853b2a536f5b391b2e93f28d4a4c93d51ff485f
alvgm98 <alvgm1998@gmail.com>
2024-07-31 14:08:00 +0200

    correccion en estilos

--- src/app/pages/appointment-form/appointment-form.component.ts
+++ src/app/pages/appointment-form/appointment-form.component.ts
@@ -1,9 +1,11 @@
-import { Component, output } from '@angular/core';
+import { Component, model, output } from '@angular/core';
+import { DatePickerComponent } from '../dashboard/components/date-picker/date-picker.component';
+import { AppointmentService } from '../../services/appointment.service';
 
 @Component({
   selector: 'app-appointment-form',
   standalone: true,
-  imports: [],
+  imports: [DatePickerComponent],
   templateUrl: './appointment-form.component.html',
   styleUrl: './appointment-form.component.scss',
   host: {
@@ -13,6 +15,20 @@ import { Component, output } from '@angular/core';
 export class AppointmentFormComponent {
 
   closeEvent = output<void>();
+
+  date = model<Date>(new Date());
+
+  constructor(
+    private appointmentService: AppointmentService
+  ) {
+    this.setSelectedDate(appointmentService.selectedDate());
+  }
+
+  setSelectedDate(date: Date) {
+    this.date.set(date);
+    console.log(date)
+    console.log(this.date())
+  }
 
   close() {
     this.closeEvent.emit();
