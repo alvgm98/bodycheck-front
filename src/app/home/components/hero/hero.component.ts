ce04abdcd5d7480870476ebabbf2c0fd67eab030
alvgm98 <alvgm1998@gmail.com>
2024-08-11 17:44:00 +0200

    Hero extraido a otro componente

---
+++ src/app/home/components/hero/hero.component.ts
@@ -0,0 +1,63 @@
+import { Component, effect, ElementRef } from '@angular/core';
+import { RegisterComponent } from '../../../auth/components/register/register.component';
+import { AuthService } from '../../../auth/services/auth.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [RegisterComponent, ReactiveFormsModule],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss'
+})
+export class HeroComponent {
+
+  showRegister: boolean = false;
+
+  constructor(
+    private el: ElementRef,
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+    effect(() => {
+      if (authService.showRegister()) {
+        this.showRegisterFn()
+      }
+      if (!authService.showRegister() && this.showRegister) {
+        this.hideRegister();
+      }
+    })
+  }
+
+  /* Mostrar y Ocultar Formulario de Registro */
+  showRegisterFn() {
+    const welcome = this.el.nativeElement.querySelector("#welcome");
+    welcome.classList.remove("pop-in-welcome");
+    welcome.classList.add("pop-out-welcome");
+
+    this.showRegister = true;
+  }
+  showRegisterFromWelcome() { // Necesario para poder actualizar estado de showRegister desde Home
+    this.authService.showRegister.set(true);
+  }
+  hideRegister() {
+    const registerModal = this.el.nativeElement.querySelector("#register-modal");
+    registerModal.classList.remove("pop-in-register");
+    registerModal.classList.add("pop-out-register");
+
+    const welcome = this.el.nativeElement.querySelector("#welcome");
+    welcome.classList.remove("pop-out-welcome");
+    welcome.classList.add("pop-in-welcome");
+
+    setTimeout(() => {
+      this.showRegister = false;
+    }, 1000)
+  }
+
+  welcomeForm = this.fb.group({
+    username: [""]
+  })
+  get username(): string {
+    return this.welcomeForm.get('username')?.value || '';
+  }
+}
