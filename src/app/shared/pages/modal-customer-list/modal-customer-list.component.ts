a79d85f862f30e8a6b2d5aa8e67df3ad1e411c23
alvgm98 <alvgm1998@gmail.com>
2024-09-19 16:50:43 +0200

    Listamos los clientes filtrados en el formulario de citas. A falta de poder seleccionarlos

--- src/app/shared/pages/modal-customer-list/modal-customer-list.component.ts
+++ src/app/shared/pages/modal-customer-list/modal-customer-list.component.ts
@@ -1,10 +1,12 @@
 import { AfterViewInit, Component, ElementRef, input, output, Renderer2, ViewChild } from '@angular/core';
 import { Customer } from '../../models/customer';
+import { CustomerService } from '../../services/customer.service';
+import { FilterCustomersPipe } from '../../pipes/filter-customers.pipe';
 
 @Component({
   selector: 'app-modal-customer-list',
   standalone: true,
-  imports: [],
+  imports: [FilterCustomersPipe],
   templateUrl: './modal-customer-list.component.html',
   styleUrl: './modal-customer-list.component.scss',
   host: {
@@ -13,11 +15,19 @@ import { Customer } from '../../models/customer';
 })
 export class ModalCustomerListComponent implements AfterViewInit {
 
-  searchFilter = input.required<string | null>();
+  customers: Customer[];
+  filterCondition = "";
+  filterConditionInput = input.required<string | null>();
 
   outputEvent = output<Customer | string>()
 
-  constructor(private el: ElementRef, private renderer: Renderer2) { }
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private customerService: CustomerService
+  ) {
+    this.customers = customerService.customers();
+  }
 
   ngAfterViewInit(): void {
     const searchInput = this.el.nativeElement.querySelector('#modalCustomerListSearchInput');
