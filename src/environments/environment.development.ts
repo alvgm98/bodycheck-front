550b01b4728a39631096a08c64c371d1a1a69796
alvgm98 <alvgm1998@gmail.com>
2024-10-26 19:49:33 +0200

    Agregada una nueva URL al enviroment.development para poder trabajar en local con varios pcs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,13 +1,15 @@
+const API_BASE_URL = 'http://localhost:8080/';
+// const API_BASE_URL = 'http://192.168.2.151:8080/';
+
 export const environment = {
   production: false,
 
   TOKEN_KEY: 'token',
   USER_KEY: 'user',
 
-  apiBaseUrl: 'http://localhost:8080/',
-  apiAuthUrl: 'http://localhost:8080/auth/',
-  apiUserUrl: 'http://localhost:8080/api/user',
-  apiCustomerUrl: 'http://localhost:8080/api/customers',
-  apiMeasurementUrl: 'http://localhost:8080/api/measurements',
-  apiAppointmentUrl: 'http://localhost:8080/api/appointments',
+  apiAuthUrl: API_BASE_URL + 'auth/',
+  apiUserUrl: API_BASE_URL + 'api/user',
+  apiCustomerUrl: API_BASE_URL + 'api/customers',
+  apiMeasurementUrl: API_BASE_URL + 'api/measurements',
+  apiAppointmentUrl: API_BASE_URL + 'api/appointments',
 };
