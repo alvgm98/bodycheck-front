776af9c8299e73ef4c691036285fc5036d851a9c
alvgm98 <alvgm1998@gmail.com>
2024-10-31 13:02:02 +0100

    Se muestran correctamente las citas en la pestaña de citas

--- src/app/customer/pages/appointments-summary/appointments-summary.component.ts
+++ src/app/customer/pages/appointments-summary/appointments-summary.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Appointment } from '../../../shared/models/appointment';
+import { CustomerAppointmentComponent } from '../../ui/customer-appointment/customer-appointment.component';
 
 @Component({
   selector: 'app-appointments-summary',
   standalone: true,
-  imports: [],
+  imports: [CustomerAppointmentComponent],
   templateUrl: './appointments-summary.component.html',
   styleUrl: './appointments-summary.component.scss'
 })
 export class AppointmentsSummaryComponent {
+
+  appointments = input.required<Appointment[]>();
 
 }
