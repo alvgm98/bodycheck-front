7284641dc6ab164898ad68646cf4f5168034db85
alvgm98 <alvgm1998@gmail.com>
2024-12-09 17:59:52 +0100

    design: Diseño para componente export-customer

--- src/app/customer/pages/export-customer/export-customer.component.ts
+++ src/app/customer/pages/export-customer/export-customer.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { SelectInputComponent } from '../../../shared/ui/select-input/select-input.component';
 
 @Component({
   selector: 'app-export-customer',
   standalone: true,
-  imports: [],
+  imports: [SelectInputComponent],
   templateUrl: './export-customer.component.html',
   styleUrl: './export-customer.component.scss'
 })
+/** Esta pagina exportará los datos del cliente seleccionado en el formato seleccionado */
 export class ExportCustomerComponent {
+  EXPORT_FORMATS = [
+    {key: "PDF", value: "PDF"},
+    {key: "JSON", value: "JSON"},
+    {key: "XML", value: "XML"},
+    {key: "CSV", value: "CSV"},
+  ]
+
+  selectedFormat: string = "";
+  exportHasError = false;
+
+  selectFormat(format: string) {
+    this.selectedFormat = format;
+    this.exportHasError = false;
+  }
+
+  export() {
+    if (!this.selectedFormat) {
+      this.exportHasError = true;
+      return;
+    }
+
+    /* EXPORTACIÓN */
+  }
 
 }
